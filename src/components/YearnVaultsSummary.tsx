7716a518b855c0f32d115c143632c3f30236b039
Ross <therossgalloway@gmail.com>
2026-03-24 09:45:39 -0400

    update vault page title

--- src/components/YearnVaultsSummary.tsx
+++ src/components/YearnVaultsSummary.tsx
@@ -1,11 +1,11 @@
 export function YearnVaultsSummary() {
-
   return (
     <div className="border border-border bg-white p-4 sm:p-6">
       {/* Header */}
-      <div className="mb-4 sm:mb-6">
-        <h1 className="text-2xl font-bold sm:text-3xl">Yearn Vaults Overview</h1>
-        <p className="text-gray-600">Aggregate info on Yearn vaults</p>
+      <div className="">
+        <h1 className="text-2xl font-bold sm:text-3xl">
+          Yearn Vaults Overview
+        </h1>
       </div>
     </div>
   )
